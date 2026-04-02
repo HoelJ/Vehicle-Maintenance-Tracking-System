12a9ee72b906708709943f5d82b9cc5139d38cc9
Ethereal113 <122694966+Ethereal113@users.noreply.github.com>
2026-04-01 23:25:18 -0400

    Updated files for SQL + changes to main site
    Added auto increment to primary keys of tables
    Implemented search bar onto homepage.
    Implemented “returning?” feature on all forms that allows for updates of rows instead of inserting new ones.
    Removed VIN from service form and custID from vehicle form to reduce potential clash of data.
    Added failsafe in case the user presses Yes for returning? But does not enter a value for ID in all forms.

---
+++ new-vehicle-form.php
@@ -0,0 +1,52 @@
+<?php
+
+    include("database.php");
+
+?>
+
+<HTML>
+
+
+
+<head>
+    <title>New Vehicle</title>
+    <h1 style="text-align: center; font-size: 75px;">New Vehicle</h1>
+</head>
+
+<body>
+    <form method="post" action="new-vehicle-record.php">
+        <form method="post" action="new-customer-record.php">
+        <fieldset>
+            <legend>Returning?</legend>
+            <div>
+                <input type="radio" id="ifReturning" name="isReturning" value="Yes" required>
+                <label for= "ifReturning"> Yes</label><br>
+                <input type="radio" id="ifNew" name="isReturning" value="No" required>
+                <label for= "ifNew"> No</label><br>
+            </div>
+        </fieldset>
+        <label style="font-size: 35px">Vehicle Identification Number:</label>
+        <input type="text" name="VIN" required>
+    <br>
+        <label style="font-size: 35px">Vehicle Type</label>
+        <input type="text" name="vehicle_type" required>
+    <br>
+        <label style="font-size: 35px">Model</label>
+        <input type="text" name="model" required>
+    <br>
+        <label style="font-size: 35px">Make</label>
+        <input type="text" name="make" required>
+    <br>
+        <label style="font-size: 35px">Year</label>
+        <input type="text" name="year" required>
+    <br>
+        <input type="submit" value="Confirm">
+    </form>
+
+         <a href="Home_Page.php" class="button-style">Go back</a>
+
+</body>
+
+
+</HTML>
+
